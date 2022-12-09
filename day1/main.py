1a773196af0d87dd4664bc26ccceee307b1c9c58
alankan886 <alankan886@gmail.com>
2022-12-09 02:09:26 -0500

    Day 1

---
+++ day1/main.py
@@ -0,0 +1,40 @@
+from heapq import heappush, heappop
+
+def first_part(calories):
+    curr_calories, largest_calories = 0, 0
+    for calorie in calories:
+        if calorie == '\n':
+            largest_calories = max(largest_calories, curr_calories)
+            curr_calories = 0
+            continue
+
+        curr_calories += int(calorie.rstrip('\n'))
+    
+    return largest_calories
+
+
+def second_part(calories):
+    min_heap = []
+    curr_calories = 0
+    for calorie in calories:
+        if calorie == '\n':
+            heappush(min_heap, curr_calories)
+            if len(min_heap) > 3:
+                heappop(min_heap)
+            curr_calories = 0
+            continue
+
+        curr_calories += int(calorie.rstrip('\n'))
+
+    return sum(min_heap)
+
+
+def main():
+    with open('inputs.txt') as f:
+        calories = f.readlines()
+        print(first_part(calories))
+        print(second_part(calories))
+
+
+if __name__ == "__main__":
+    main()
